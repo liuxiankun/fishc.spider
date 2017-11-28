2c0b2f19b8aa4c1d8272df1eac685d3af6076e9e
刘显坤 <liuxiank@yeah.net>
2017-11-28 11:03:15 +0800

    third

--- download/downloadPicture.py
+++ download/downloadPicture.py
@@ -7,31 +7,47 @@ download picture
 
 import urllib.request
 import random
-from  http.cookiejar import CookieJar
+from http.cookiejar import CookieJar
 import os
 import re
 
 
 def url_open(url):
+
+    '''
+    请求一个网址，返回页面信息
+    :param url:
+    :return:
+    '''
+
+    #设置代理
     iplist = ['114.215.102.168:8081', '121.40.199.105:80', '123.103.93.38:80', '116.199.2.209:80', '116.199.2.210',
               '121.40.213.161:80']
     proxy_support = urllib.request.ProxyHandler({'http': random.choice(iplist)})
 
     open = urllib.request.build_opener(proxy_support)
+
     open.addheaders = [('User-Agent',
                           'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')]
     urllib.request.install_opener(open)
 
+
+
     headers = {'User-Agent':
                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
                }
-    req=urllib.request.Request(url, None, headers)
+
+    #建立有cookie的请求
+    #在 HTTP Request 中加入特定的 Header 要加入 header，需要使用 Request 对象：
+
+    request = urllib.request.Request(url, None, headers)
     cj = CookieJar()
     opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
-    response = opener.open(req)
+    response = opener.open(request)
     html=response.read()
     #print(url)
     return html
+
 
 def get_page(url):
     html = url_open(url).decode('utf-8')
@@ -54,7 +70,7 @@ def find_imgs(url):
         urllib.request.urlretrieve('http:'+each, filename, None)
 
 
-def download_mm( folder, pages=3 ):
+def download_mm(folder, pages=3):
     if not os.path.exists(folder):
         os.system('mkdir %s'%folder)
     os.chdir(folder)
